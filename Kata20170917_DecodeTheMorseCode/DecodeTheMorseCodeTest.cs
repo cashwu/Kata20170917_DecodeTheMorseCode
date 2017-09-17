7ddb6e605ae1f7b5da244817981b55f96ecb991c
cash <cash@cashwu.com>
2017-09-17 20:50:58 +0800

    input A test case

--- Kata20170917_DecodeTheMorseCode/DecodeTheMorseCodeTest.cs
+++ Kata20170917_DecodeTheMorseCode/DecodeTheMorseCodeTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20170917_DecodeTheMorseCode
@@ -7,8 +8,34 @@ namespace Kata20170917_DecodeTheMorseCode
     public class DecodeTheMorseCodeTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test_input_A()
         {
+            MorseCodeDecoderShouldBe("A", ".-");
+        }
+
+        private static void MorseCodeDecoderShouldBe(string expected, string morseCode)
+        {
+            var morseCodeDecoder = new MorseCodeDecoder();
+            var actual = morseCodeDecoder.Decode(morseCode);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class MorseCodeDecoder
+    {
+        private readonly Dictionary<string, string> MorseCode;
+
+        public MorseCodeDecoder()
+        {
+            MorseCode = new Dictionary<string, string>
+            {
+                {".-", "A"}
+            };
+        }
+
+        public string Decode(string morseCode)
+        {
+            return MorseCode[morseCode];
         }
     }
 }
