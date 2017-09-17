1c9b364f66d964ebb8b47f8295f79a256670dcb7
cash <cash@cashwu.com>
2017-09-17 21:14:00 +0800

    refactor

--- Kata20170917_DecodeTheMorseCode/DecodeTheMorseCodeTest.cs
+++ Kata20170917_DecodeTheMorseCode/DecodeTheMorseCodeTest.cs
@@ -20,6 +20,18 @@ namespace Kata20170917_DecodeTheMorseCode
             MorseCodeDecoderShouldBe("AB", ".- -...");
         }
 
+        [TestMethod]
+        public void Test_input_ABC()
+        {
+            MorseCodeDecoderShouldBe("ABC", ".- -... -.-.");
+        }
+
+        [TestMethod]
+        public void Test_input_ABC_A()
+        {
+            MorseCodeDecoderShouldBe("ABC A", ".- -... -.-.   .-");
+        }
+
         private static void MorseCodeDecoderShouldBe(string expected, string morseCode)
         {
             var morseCodeDecoder = new MorseCodeDecoder();
@@ -30,28 +42,28 @@ namespace Kata20170917_DecodeTheMorseCode
 
     public class MorseCodeDecoder
     {
-        private readonly Dictionary<string, string> MorseCode;
-
-        public MorseCodeDecoder()
+        private static readonly Dictionary<string, string> MorseCode = new Dictionary<string, string>
         {
-            MorseCode = new Dictionary<string, string>
-            {
-                {".-", "A"}, {"-...", "B"},{ "-.-.", "C"}, {"-..", "D"}, {".", "E"},
-                {"..-.", "F" }, {"--.", "G"}, {"....", "H"}, {"..", "I"}, {".---", "J"},
-                {"-.-", "K" }, {".-..", "L"}, {"--", "M"}, {"-.", "N"}, {"---", "O"},
-                {".--.", "P" }, {"--.-", "Q"}, {".-.", "R"}, {"...", "S"}, {"-", "T"},
-                {"..-", "U" }, {"...-", "V"}, {".--", "W"}, {"-..-", "X"}, {"-.--", "Y"},
-                {"--..", "Z" }, {"-----", "0"}, {".----", "1"}, {"..---", "2"}, {"...--", "3"},
-                {"....-", "4" }, {".....", "5"}, {"-....", "6"}, {"--...", "7"}, {"---..", "8"},
-                {"----.", "9" }
-            };
-        }
+            {".-", "A"}, {"-...", "B"},{ "-.-.", "C"}, {"-..", "D"}, {".", "E"},
+            {"..-.", "F" }, {"--.", "G"}, {"....", "H"}, {"..", "I"}, {".---", "J"},
+            {"-.-", "K" }, {".-..", "L"}, {"--", "M"}, {"-.", "N"}, {"---", "O"},
+            {".--.", "P" }, {"--.-", "Q"}, {".-.", "R"}, {"...", "S"}, {"-", "T"},
+            {"..-", "U" }, {"...-", "V"}, {".--", "W"}, {"-..-", "X"}, {"-.--", "Y"},
+            {"--..", "Z" }, {"-----", "0"}, {".----", "1"}, {"..---", "2"}, {"...--", "3"},
+            {"....-", "4" }, {".....", "5"}, {"-....", "6"}, {"--...", "7"}, {"---..", "8"},
+            {"----.", "9" }
+        };
 
         public string Decode(string morseCode)
         {
-            var morseCodeCharArray = morseCode.Split(' ');
+            var morseCodeWords = morseCode.Split(new []{ "   " }, StringSplitOptions.RemoveEmptyEntries);
+            return string.Join(" ", morseCodeWords.Select(DecodeWord));
+        }
 
-            return string.Concat(morseCodeCharArray.Select(c => MorseCode[c]));
+        public static string DecodeWord(string word)
+        {
+            var chars = word.Split(' ');
+            return string.Concat(chars.Select(c => MorseCode[c]));
         }
     }
 }
